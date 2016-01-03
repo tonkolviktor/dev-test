43b16aa38ac24955239c9bcbd5c4817338dfb509
tonkol <tonkol.viktor@danubiusinfo.hu>
2016-01-03 15:56:46 +0100

    create argument class

--- src/main/java/hu/tonkol/devtest/Application.java
+++ src/main/java/hu/tonkol/devtest/Application.java
@@ -1,11 +1,31 @@
 package hu.tonkol.devtest;
 
+import com.sun.org.apache.xpath.internal.Arg;
+
 /**
  * Created by tonkol on 2016.01.03..
  */
 public class Application {
 
+    private Argument argument;
+
     public static void main(String[] args) {
-        System.out.println("Hello console!");
+        try {
+            new Application().run(args);
+        } catch (BusinessException be) {
+            System.out.println(be.getMessage());
+            printHelp();
+        } catch (Exception e) {
+            System.out.println("Unknown error occurred: " + e.getMessage());
+            printHelp();
+        }
+    }
+
+    private static void printHelp() {
+        System.out.println("Usage: java -jar <jar-name> <city>");
+    }
+
+    public void run(String[] args) {
+        this.argument = new Argument(args);
     }
 }
--- src/test/java/hu/tonkol/devtest/ApplicationTest.java
+++ src/test/java/hu/tonkol/devtest/ApplicationTest.java
@@ -1,18 +1,61 @@
 package hu.tonkol.devtest;
 
+import org.junit.Rule;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by tonkol on 2016.01.03..
  */
 public class ApplicationTest {
 
+    private ByteArrayOutputStream outContent;
+
+    @BeforeMethod
+    public void setUpStreams() {
+        outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterMethod
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
     @Test
     public void shouldMainRequireOneArgument() {
         //given
         //when
-        Application.main(new String[0]);
+        Application.main(new String[]{"Berlin"});
         //then
+        assertThat(outContent.toString()).isEqualTo("");
     }
+
+    @Test
+    public void shouldMainPrintErrorMessageOnBusinessException() {
+        //given
+        //when
+        Application.main(null);
+        //then
+        assertThat(outContent.toString()).contains("At least one argument must be specified!");
+    }
+
+    @Test
+    public void shouldMainPrintHelpOnBusinessException() {
+        //given
+        //when
+        Application.main(null);
+        //then
+        assertThat(outContent.toString()).contains("Usage: java -jar <jar-name> <city>");
+    }
+
 
 }
