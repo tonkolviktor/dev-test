bec78a71a6742e5e7a18d412efe444f31a301277
tonkol <tonkol.viktor@danubiusinfo.hu>
2016-01-03 14:46:12 +0100

    maven config main skeleton

---
+++ src/main/java/hu/tonkol/devtest/Application.java
@@ -0,0 +1,11 @@
+package hu.tonkol.devtest;
+
+/**
+ * Created by tonkol on 2016.01.03..
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello console!");
+    }
+}
---
+++ src/test/java/hu/tonkol/devtest/ApplicationTest.java
@@ -0,0 +1,18 @@
+package hu.tonkol.devtest;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by tonkol on 2016.01.03..
+ */
+public class ApplicationTest {
+
+    @Test
+    public void shouldMainStartApplication() {
+        //given
+        //when
+        Application.main(new String[0]);
+        //then
+    }
+
+}
